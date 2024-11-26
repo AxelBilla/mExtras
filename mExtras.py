dcada1fce123a230397d41bbb50c5816537140ee
AxelBilla <144888254+AxelBilla@users.noreply.github.com>
2024-11-26 23:45:14 +0100

    Add files via upload

--- mExtras.py
+++ mExtras.py
@@ -1,4 +1,4 @@
-#bot url
+#https://discord.com/oauth2/authorize?client_id=1311068031384027167
 
 import os
 import discord
@@ -21,21 +21,9 @@ def addPath(ctx):
         os.makedirs(path)
         open(path+"/data.txt","w")
         file = open(path+"/data.txt", "w")
-        word=["[ROLE]: active\n"]
+        word=["[ROLE]: none\n"]
         file.writelines(word)
         file.close()
-
-#syncs the commands
-@bot.tree.command(name='sync', description='| BOT OWNER ONLY |')
-async def sync(ctx):
-    print(f"\n\n\n{ctx.user.name} is trying to access SYNC. [ID]: {ctx.user.id}, [SERVER]: {ctx.guild.name}")
-    if ctx.user.id == owner_id:
-        await bot.tree.sync()
-        print("They're this bot's owner.\n")
-        await ctx.response.send_message('synced.', ephemeral=True)
-    else:
-        print("They're not this bot's owner")
-        await ctx.response.send_message('You do not have the permission(s) required to do this.', ephemeral=True)
 
 #checks if admin or bot owner (debug purposes)
 @bot.event
@@ -57,11 +45,18 @@ async def owner_admin(ctx): #Allows me to debug stuff at will.
     a=await predicate(ctx)
     return app_commands.check(a) 
 
-
-
-
-
-
+#syncs the commands
+@bot.tree.command(name='sync_act', description='| BOT OWNER ONLY |')
+@app_commands.check(owner_admin)
+async def sync(ctx):
+    print(f"\n\n\n{ctx.user.name} is trying to access SYNC. [ID]: {ctx.user.id}, [SERVER]: {ctx.guild.name}")
+    if ctx.user.id == owner_id:
+        await bot.tree.sync()
+        print("They're this bot's owner.\n")
+        await ctx.response.send_message('synced.', ephemeral=True)
+    else:
+        print("They're not this bot's owner")
+        await ctx.response.send_message('You do not have the permission(s) required to do this.', ephemeral=True)
 
 #gives a default role to be given (if it doesn't exist)
 @bot.tree.command(name='default_act', description='[ADMIN] Adds a default access role ("admin") to pre-existing servers.')
@@ -91,63 +86,67 @@ async def setupAllowedRole(ctx, role: str):
     wordList=file.readlines()
     file.close()
     rolename=wordList[0].strip("\n").replace("[ROLE]: ", "")
-    print(f'[/role_disk]\n[ORIGINAL ROLE]: "{rolename}"\n[NEW ROLE]: "{role}"')
+    print(f'[/role_act]\n[ORIGINAL ROLE]: "{rolename}"\n[NEW ROLE]: "{role}"')
     wordList[0]="[ROLE]: "+role+"\n"
     fileWrite = open(dir+"/serverData/" + str(ctx.guild.id) + "/" + "data.txt", "w")
     fileWrite.writelines(wordList)
     fileWrite.close()
+    await hourlyCheck.start(ctx)
     await ctx.response.send_message(f"\"{role}\" has been made the server's only role with access to the advanced commands!", ephemeral=True)
-
-
-
-
-
 
 #checks if user has sent at least once message in the last 7 days
 @tasks.loop(hours=1)
 async def hourlyCheck(ctx):
-    addPath(ctx)
     file = open(dir+"/serverData/" + str(ctx.guild.id) + "/" + "data.txt", "r")
     userList=file.readlines()
     file.close()
-    for i in range(1, len(userList)-1):
-        nowDate = datetime.datetime.now()
-        content=userList[i].split
-        userID=discord.utils.get(ctx.guild.members, id=content[0].strip("[]:"))
-        savedDate=datetime.datetime.strptime(content[1], "%d/%m/%Y")
-        timeGap = nowDate - savedDate
-        if timeGap.days > 7:
-            role=userList[0].split
-            role=role[1]
-            role=discord.utils.get(ctx.guild.roles, name=role)
-            await userID.remove_roles(role)
+    roleCheck=userList[0].split()
+    roleCheck=roleCheck[1]
+    if roleCheck != "none":
+        for i in range(1,len(userList)):
+            now=datetime.now()
+            nowDate = now.strptime(now.strftime("%d/%m/%Y"), "%d/%m/%Y")
+            content=userList[i].split()
+            savedDate= now.strptime(content[1], "%d/%m/%Y")
+            timeGap = nowDate - savedDate
+            if timeGap.days > 7:
+                role=userList[0].split()
+                role=role[1]
+                role=discord.utils.get(ctx.guild.roles, name=role)
+                member = await ctx.guild.fetch_member(content[0].strip("[]:"))
+                await member.remove_roles(role)
 
 #updates upon user message and gives them the role if they don't have it
-#on message sent
-async def onMessageUpdate(ctx):
+@bot.event
+async def on_message(ctx):
     addPath(ctx)
     file = open(dir+"/serverData/" + str(ctx.guild.id) + "/" + "data.txt", "r")
     userList=file.readlines()
     file.close()
-    for i in range(1, len(userList)-1):
-        content=userList[i].split()
-        userID=content[0].strip("[]:")
-        if userID==ctx.user.id:
-            content[1]=datetime.now().strftime("%d/%m/%Y")
-            userList[i]=' '.join(content)+'\n'
-            fileWrite = open(dir+"/serverData/" + str(ctx.guild.id) + "/" + "data.txt", "w")
-            fileWrite.writelines(userList)
-            fileWrite.close()
-            check=False
-            break
-    if check:
-        nowDate=datetime.now().strftime("%d/%m/%Y")
-        fileWrite = open(dir+"/serverData/" + str(ctx.guild.id) + "/" + "data.txt", "a")
-        fileWrite.write(f"[{ctx.user.id}]: {nowDate}\n")
-        fileWrite.close()
-    role=userList[0].split
-    role=role[1]
-    role=discord.utils.get(ctx.guild.roles, name=role)
-    await ctx.user.add_roles(role)
+    roleCheck=userList[0].split()
+    roleCheck=roleCheck[1]
+    check=True
+    if roleCheck != "none":
+        for i in range(len(userList)):
+            content=userList[i].split()
+            userID=content[0].strip("[]:")
+            if userID==str(ctx.author.id):
+                content[1]=datetime.now().strftime("%d/%m/%Y")
+                userList[i]=' '.join(content)+'\n'
+                fileWrite = open(dir+"/serverData/" + str(ctx.guild.id) + "/" + "data.txt", "w")
+                fileWrite.writelines(userList)
+                fileWrite.close()
+                check=False
+                break
+            if check:
+                nowDate=datetime.now().strftime("%d/%m/%Y")
+                fileWrite = open(dir+"/serverData/" + str(ctx.guild.id) + "/" + "data.txt", "a")
+                fileWrite.write(f"[{ctx.author.id}]: {nowDate}\n")
+                fileWrite.close()
+        role=userList[0].split()
+        role=role[1]
+        role=discord.utils.get(ctx.guild.roles, name=role)
+        member = await ctx.guild.fetch_member(ctx.author.id)
+        await member.add_roles(role)
 
 bot.run(token)
